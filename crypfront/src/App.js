3dc1cc2736818f95a8f8e25f4e6f2df09e684a10
LEMAISSI Samy <samy.lemaissi@atos.net>
2020-02-05 12:36:14 +0100

    add dashboard

--- crypfront/src/App.js
+++ crypfront/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
  import './App.css';
 import LineChart from './Component/LineChart';
+import Dashboard from './Component/Dashboard';
 
 function App() {
   return (
     <div className="App">
+             <Dashboard />
        <LineChart />
     </div>
   );
