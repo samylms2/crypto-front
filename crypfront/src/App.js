b42a2fe8242e84ace5912847423253674ebc90b0
LEMAISSI Samy <samy.lemaissi@atos.net>
2020-02-05 16:32:15 +0100

    hide show child component

--- crypfront/src/App.js
+++ crypfront/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
  import './App.css';
-import LineChart from './Component/LineChart';
-import Dashboard from './Component/Dashboard';
+ import Dashboard from './Component/Dashboard';
 
 function App() {
   return (
     <div className="App">
              <Dashboard />
-       <LineChart />
     </div>
   );
 }
--- crypfront/src/Component/Dashboard.js
+++ crypfront/src/Component/Dashboard.js
@@ -1,39 +1,51 @@
 import React, { Component } from 'react';
 import './dashboard.css';
 import axios from 'axios';
- 
+import LineChart from './LineChart';
+
+
 class Dashboard extends Component {
 
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-          cryptos: []
-        };
-      }
+  constructor(props) {
+    super(props);
 
-  
+    this.state = {
+      cryptos: [],
+      showLineChart: false
+    }
+  }
 
-  componentDidMount() {
-    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD')
-    .then(res => {
-      const cryptos = res.data;
-      console.log(cryptos);
-      this.setState({cryptos: cryptos});
+  _showLineChart = (bool) => {
+    this.setState({
+      showLineChart: bool
     })
   }
 
-   
+  componentDidMount() {
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=EUR')
+      .then(res => {
+        const cryptos = res.data;
+        console.log(cryptos);
+        this.setState({ cryptos: cryptos });
+      })
+  }
+
+
 
   render() {
     return (
-        <div className="App">
+      <div className="App">
         {Object.keys(this.state.cryptos).map((key) => (
           <div id="crypto-container">
             <span className="left">{key}</span>
-            <span className="right">{this.state.cryptos[key].EUR}</span>
+            <span className="right">{this.state.cryptos[key].EUR}€</span>
           </div>
         ))}
+        <button onClick={this._showLineChart.bind(null, true)}>show</button>
+        <button onClick={this._showLineChart.bind(null, false)}>hide</button>
+        {this.state.showLineChart && (<LineChart />)}
+
+
       </div>
     );
   }
